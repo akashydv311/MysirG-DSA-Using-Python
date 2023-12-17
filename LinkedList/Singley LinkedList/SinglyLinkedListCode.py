e3c61fa5277dcfa1e29aa5995e5a34870ade74d0
akashydv311 <akashydv311@gmail.com>
2023-12-17 06:22:44 +0530

    singley linked list code updated

--- LinkedList/Singley LinkedList/SinglyLinkedListCode.py
+++ LinkedList/Singley LinkedList/SinglyLinkedListCode.py
@@ -41,17 +41,60 @@ class SLL:
         # search fail / empty list
         return None
     
+    def insert_after(self, temp, data):
+        # assume refrence given(temp)by serach
+        if temp is not None:
+            n = Node(data, temp.next)
+            temp.next = n
+    
     def print_list(self):
         temp = self.start
         while(temp != None):
             print(temp.iteam, end=' ')
             temp = temp.next
 
+    def delete_first(self):
+        if self.start is not None:
+            self.start = self.start.next
+        # temp = self.start
+        # if not self.is_empty():
+        #     self.start = temp.next
+
+    def delete_last(self):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            self.start = None
+        else:
+            temp = self.start
+            prev = temp
+            while temp.next is not None:
+                prev = temp
+                temp = temp.next
+            prev.next = None
+
+    def delete_item(self, data):
+        if self.start is None:
+            pass
+        elif self.start.iteam == data:
+            self.start = None
+        else:
+            temp = self.start
+            prev = temp
+            while temp.next is not None:
+                if temp.iteam == data:
+                
+
+
 # Driver Code
 mylist = SLL()
 mylist.insert_at_start(23)
 mylist.insert_at_last(78)
 mylist.insert_at_start(89)
+mylist.insert_after(mylist.search(78), 100)
+mylist.insert_after(mylist.search(79), 200)
+mylist.insert_at_last(679)
+
 mylist.print_list()
 
 
